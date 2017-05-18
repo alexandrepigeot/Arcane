5c0ea534fb82925316a009703bfec11574eddf50
Alexandre Pigeot <Alexandre Pigeot>
2017-05-18 11:50:27 +0400

    Rearrange folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,36 @@ var pfix = require("gulp-autoprefixer");
 var ts = require("gulp-typescript");
 var web = require("gulp-webserver");
 
-var files = {
+var path = {
 	sass: "./src/**/*.scss",
-	ts: "./src/**/*.ts"
+	ts: "./src/**/*.ts",
+	output: "./content",
+	web: "./"
 }
 
 gulp.task("default", ["run-sass", "run-ts"], function () {
-	gulp.src("./")
+	gulp.src(path.web)
 		.pipe(web({
 			livereload: true,
-			directoryListing: true,
-			open: true
+			directoryListing: false,
+			open: true,
+			fallback: "index"
 		}));
-	gulp.watch(files.sass, ["run-sass"]);
-	gulp.watch(files.ts, ["run-ts"]);
+	gulp.watch(path.sass, ["run-sass"]);
+	gulp.watch(path.ts, ["run-ts"]);
 });
 
 gulp.task("run-sass", function () {
-	gulp.src(files.sass)
+	gulp.src(path.sass)
 		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
 		.pipe(pfix({ browsers: ["last 2 versions"] }))
-		.pipe(gulp.dest("./"));
+		.pipe(gulp.dest(path.output));
 });
 
 gulp.task("run-ts", function () {
-	gulp.src(files.ts)
+	gulp.src(path.ts)
 		.pipe(ts({
 			noImplicitAny: true
 		}))
-		.pipe(gulp.dest("./"));
+		.pipe(gulp.dest(path.output));
 })
